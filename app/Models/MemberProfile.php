c7676077f14e76f62c58a41f75590a5d9412c575
satoshiyr16 <satoshi.trip.1@gmail.com>
2025-07-07 23:51:40 +0900

    [add]メンバー情報用のmodel作成

---
+++ app/Models/MemberProfile.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\Common\GenderEnum;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class MemberProfile extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'user_id',
+        'nickname',
+        'gender',
+        'birth_date',
+        'enrollment_date',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'gender' => GenderEnum::class,
+            'birth_date' => 'date',
+            'enrollment_date' => 'date',
+        ];
+    }
+
+    /**
+     * ユーザーとのリレーション
+     * 
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
